d9df614edbf36bcaff7f00ab614c0e3b43990a18
Diego Portillo <dportillodeveloper@gmail.com>
2026-01-31 22:05:57 -0300

    n-ary tree deep first search

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # study-universe-project
+
 This project was made to apply all my software studies in a mock start-up
+
+Technologies
+
+- Python
+
+Topics:
+
+- N-ary Tree Depth First Search
---
+++ folder_exists_script.py
@@ -0,0 +1,37 @@
+def folder_exists(folder, folder_name):
+    if folder == []:
+        return False
+    print(f"Folder file: {folder['name']}")
+    if folder['name'] == folder_name:
+        return True
+    for child_folder in folder['childs']:
+        folder_found = folder_exists(child_folder, folder_name)
+        if folder_found == True:
+            return folder_found
+    return False
+
+
+root_folder = {
+    "name": "root",
+    "childs": [
+        {
+            "name": "user1",
+            "childs": [
+                {"name": "documents", "childs": []},
+                {"name": "images", "childs": []},
+                {"name": "downloads", "childs": [{"name": "virus2", "childs": []}]},
+            ],
+        },
+        {
+            "name": "user2",
+            "childs": [
+                {"name": "documents", "childs": []},
+                {"name": "images", "childs": []},
+                {"name": "downloads", "childs": []},
+            ],
+        },
+    ],
+}
+
+folder_to_find = "virus"
+print(f"Folder {folder_to_find} exists? {folder_exists(root_folder, folder_to_find)}")
